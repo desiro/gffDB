fcbbf24e2971aff030b14d6fcf0f08073f5242e4
desiro <desiro>
2015-05-23 22:29:51 +0200

    rework

--- db_to_gff.py
+++ db_to_gff.py
@@ -12,154 +12,53 @@ Source:
     https://github.com/desiro/gffDB/blob/master/db_to_gff.py
 
 """
-import os
-import sys
-import re
 import gffutils
 import argparse
 
 
 ## main function
-def main(gffFile, dataBase, all, features, fCount, fTypes, source, seqid, start, end, notes, schema, strand, order, reverse, within):
+def main(gffFile, dataBase, batchSearch, all, features, source, seqid, start, end, strand, within):
     # opening the database
-    db = gffutils.FeatureDB(dbfn=dataBase)
-    # initialize parameters
-    featureList = None
-    orderList = None
-    searchTuple = None
-    data = None
+    try:
+        db = gffutils.FeatureDB(dbfn=dataBase)
+    except:
+        print("can't open database")
     
-    # processing the feature list
-    if features is not None:
-        featureList = features.split(',')
-    # process the order list
-    if order is not None:
-        orderList = order.split(',')
-    # create search tuple if possible
-    if seqid is not None and start is not None and end is not None:
-        searchTuple = seqid + ':' + start + '-' + end
-    
-    # decides the main task
-    if all is True or (order is not None and searchTuple is None and (seqid is not None or start is not None or strand != 'none')):
-        data = searchAll(db, searchTuple, strand, featureList, orderList, reverse, within)
-    elif order is None and (seqid is not None or start is not None or end is not None):
-        data = searchRegion(db, seqid, start, end, strand, featureList, within)
-    elif featureList is not None:
-        data = searchFeatures(db, featureList, searchTuple, strand, orderList, reverse, within)
-    elif source is not None: 
-        data = searchAll(db, searchTuple, strand, featureList, orderList, reverse, within)
+    # getting all data
+    if all:
+        data = db.all_features()
+        printData(data, gffFile, source, 'w')
+    # getting data from batch file
+    elif batchSearch:
+        with open(batchSearch,'r') as batch:
+            first = True
+            for line in batch:
+                b_feature, b_seqid, b_start, b_end, b_strand, b_within = line.split('\t')
+                if b_within == 't' or b_within == 'true' or b_within == 'T' or b_within == 'True' or b_within == 'w' or b_within == 'within':
+                    b_within_bool = True
+                else:
+                    b_within_bool = False
+                data = db.region(seqid=b_seqid, start=b_start, end=b_end, strand=b_strand, featuretype=b_feature, completely_within=b_within_bool)
+                if first:
+                    printData(data, gffFile, source, 'w')
+                    first = False
+                else:
+                    printData(data, gffFile, source, 'a')
+    # getting single request
     else:
-        schema = True
-    
-    # print data
-    printData(db, data, gffFile, dataBase, all, features, fCount, fTypes, source, seqid, start, end, notes, schema, strand, order, reverse, within, searchTuple)
-
-
-## get all the data
-def searchAll(db, searchTuple, strand, featureList, orderList, reverse, within):
-    data = db.all_features(limit=searchTuple, strand=strand, featuretype=featureList, order_by=orderList, reverse=reverse, completely_within=within)
-    return data
-
-
-## get region
-def searchRegion(db, seqid, start, end, strand, featureList, within):
-    data = db.region(seqid=seqid, start=start, end=end, strand=strand, featuretype=featureList, completely_within=within)
-    return data
-
-
-## get features
-def searchFeatures(db, featureList, searchTuple, strand, orderList, reverse, within):
-    # get feature
-    data = db.features_of_type(featureList, limit=searchTuple, strand=strand, order_by=orderList, reverse=reverse, completely_within=within)
-    return data
+        featureList = None
+        if features:
+            featureList = features.split(',')
+        data = db.region(seqid=seqid, start=start, end=end, strand=strand, featuretype=featureList, completely_within=within)
+        printData(data, gffFile, source, 'w')
 
 
 ## print data
-def printData(db, data, gffFile, dataBase, all, features, fCount, fTypes, source, seqid, start, end, notes, schema, strand, order, reverse, within, searchTuple):
+def printData(data, gffFile, source, write):
     # open the gff file
-    with open(gffFile,'w') as file:
-        
-        # prints header data
-        file.write("# tool: https://github.com/desiro/gffDB/blob/master/db_to_gff.py" + "\n")
-        file.write("# database: " + dataBase + "\n")
-        if notes is not None:
-            file.write("# " + "\n")
-            file.write("# notes: " + "\n")
-            commentList = notes.split('#')
-            for i in commentList:
-                file.write("#     " + i + "\n")
-        
-        # prints search data
-        file.write("# " + "\n")
-        file.write("# search: " + "\n")
-        if all is True:
-            file.write("#     --all = True" + "\n")
-        if features is not None:
-            file.write("#     --features = " + features + "\n")
-        if seqid is not None:
-            file.write("#     --seqid = " + seqid + "\n")
-        if start is not None:
-            file.write("#     --start = " + start + "\n")
-        if end is not None:
-            file.write("#     --end = " + end + "\n")
-        if strand != 'none':
-            file.write("#     --strand = " + strand + "\n")
-        if order is not None:
-            file.write("#     --order = " + order + "\n")
-        if reverse is True:
-            file.write("#     --reverse = True" + "\n")
-        if within is True:
-            file.write("#     --within = True" + "\n")
-        if source is not None:
-            file.write("#     --source = " + source + "\n")
-        if schema is True:
-            file.write("#     --schema = True" + "\n")
-        if fTypes is True:
-            file.write("#     --fTypes = True" + "\n")
-        if fCount is not None:
-            file.write("#     --fCount = " + fCount + "\n")
-        
-        # prints additional requested database information
-        if fTypes is True:
-            file.write("# " + "\n")
-            featureTypes = db.featuretypes()
-            printFtypes = ""
-            for i in featureTypes:
-                printFtypes += str(i)
-                printFtypes += ", "
-            file.write("# feature types: " + printFtypes +  "\n")
-        if fCount is not None:
-            file.write("# " + "\n")
-            if fCount == 'all':
-                fCount = None
-            countFeatures = db.count_features_of_type(featuretype=fCount)
-            file.write("# feature count: " + str(countFeatures) + "\n")
-        if schema is True:
-            file.write("# " + "\n")
-            printSchema = db.schema().split('\n')
-            file.write("# database schema: " + "\n")
-            for i in printSchema:
-                file.write("#     " + i + "\n")
-        
-        # prints gff data
-        file.write("# " + "\n")
+    with open(gffFile,write) as file:
         for entry in data:
-            if searchTuple is None and seqid is not None and entry.seqid != seqid:
-                try:
-                    next(data)
-                except StopIteration:
-                    pass
-            elif searchTuple is None and start is not None and ((within is True and (int(entry.start) < int(start))) or (within is False and (int(entry.end) <= int(start)))):
-                try:
-                    next(data)
-                except StopIteration:
-                    pass
-            elif searchTuple is None and end is not None and ((within is True and (int(entry.end) > int(end))) or (within is False and (int(entry.start) >= int(end)))):
-                try:
-                    next(data)
-                except StopIteration:
-                    pass
-            elif source is not None and entry.source != source:
+            if source and entry.source != source:
                 try:
                     next(data)
                 except StopIteration:
@@ -174,43 +73,16 @@ if __name__ == '__main__':
     parser.add_argument('--version', action='version', version='%(prog)s 0.1')
     parser.add_argument('--gff', '-g', dest='gffFile', required=True, help='GFF file name')
     parser.add_argument('--database', '-d', dest='dataBase', required=True, help='gffutils database')
+    parser.add_argument('--batch', '-b', dest='batchSearch', help='tabular file with search data; format: feature - seqid - start - end - strand - within; values can be empty; always provide 5 tabs; use t/T/true/True/w/within for within, let it empty or something else for not within')
     parser.add_argument('--all', '-a', dest='all', action='store_true', help='returns all db entries')
     parser.add_argument('--features', '-f', dest='features', help='returns all entries with the requested features; can be a comma separated list')
-    parser.add_argument('--fCount', '-fn', dest='fCount', help='returns the number occurrences of the requested feature at the top of the gff file; use \'all\' for counting all features in the database')
-    parser.add_argument('--fTypes', '-ft', dest='fTypes', action='store_true', help='returns all feature types in the database at the top of the gff file')
     parser.add_argument('--source', '-so', dest='source', help='returns features with the requested source')
     parser.add_argument('--seqid', '-sq', dest='seqid', help='returns features with the requested seqid')
     parser.add_argument('--start', '-s', dest='start', help='only returns features that start with this region or after this region')
     parser.add_argument('--end', '-e', dest='end', help='only returns features that end with this region or before this region')
-    parser.add_argument('--notes', '-n', dest='notes', help='adds specific notes at the top of the gff file; provide the comment in quotation mark and use # for new line')
-    parser.add_argument('--schema', '-sc', dest='schema', action='store_true', help='returns the schema of the database at the top of the gff file, this is also the default output when you do not specify anything')
-    parser.add_argument('--strand', '-r', dest='strand', default='none', choices=['none', '+', '-', '.'], help='returns only features in strand direction; \'.\' returns unstranded features')
-    parser.add_argument('--order', '-o', dest='order', help='order results by fields; must be a comma separated list of field names (seqid, source, featuretype, start, end, score, strand, frame, attributes, extra); could slow down the search')
-    parser.add_argument('--reverse', '-v', dest='reverse', action='store_true', help='sort in descending order; only use with --order option')
-    parser.add_argument('--within', '-w', dest='within', action='store_true', help='forces the feature to be completely within the provided --start and/or --end option')
+    parser.add_argument('--strand', '-r', dest='strand', choices=['+', '-', '.'], help='returns only features in strand direction; \'.\' returns unstranded features')
+    parser.add_argument('--within', '-w', dest='within', action='store_true', help='forces the feature to be completely within the provided --start and/or --end region')
     
     options = parser.parse_args()
-    main(options.gffFile, options.dataBase, options.all, options.features, options.fCount, options.fTypes, options.source, options.seqid, options.start, options.end, options.notes, options.schema, options.strand, options.order, options.reverse, options.within)
-
-
-# optional:
-# ---------
-# 
-# Interact with a FeatureDB:
-# gffutils.FeatureDB.children(id[, level, ...]) 	Return children of feature id.
-# gffutils.FeatureDB.parents(id[, level, ...]) 	Return parents of feature id.
-# gffutils.FeatureDB.execute(query) 	Execute arbitrary queries on the db.
-# gffutils.FeatureDB.iter_by_parent_childs([...])
-# 
-# Modify a FeatureDB:
-# gffutils.FeatureDB.delete(features[, ...]) 	Delete features from database.
-# gffutils.FeatureDB.add_relation(parent, ...) 	Manually add relations to the database.
-# gffutils.FeatureDB.set_pragmas(pragmas) 	Set pragmas for the current database connection.
-# 
-# Operate on features:
-# gffutils.FeatureDB.interfeatures(features[, ...]) 	Construct new features representing the space between features.
-# gffutils.FeatureDB.children_bp(feature[, ...]) 	Total bp of all children of a featuretype.
-# gffutils.FeatureDB.merge(features[, ...]) 	Merge overlapping features together.
-# gffutils.FeatureDB.create_introns([...]) 	Create introns from existing annotations.
-# gffutils.FeatureDB.bed12(feature[, ...]) 	Converts feature into a BED12 format.
+    main(options.gffFile, options.dataBase, options.batchSearch, options.all, options.features, options.source, options.seqid, options.start, options.end, options.strand, options.within)
 
--- gff_to_db.py
+++ gff_to_db.py
@@ -6,68 +6,53 @@ Description:
     Convert a gff file into a new database or add it to a existing database.
 
 Usage:
-    gff_to_db.py -g <gff_file> -d <database_file>
+    gff_to_db.py -g <gff_file> -i <sqlite_file> 
 
 Source:
     https://github.com/desiro/gffDB/blob/master/gff_to_db.py
 
 """
 import os
-import sys
-import re
 import gffutils
 import argparse
 
 
-def main(gffFile, dataBase, forceOv, backup, idSpec, mergeStrat, fieldKey):
+def main(gffFile, inputDB, newDB):
     # creating a new database or adding to an excising one
-    if not os.path.isfile(dataBase) or forceOv is True:
-        createDB(gffFile,dataBase,forceOv,idSpec,mergeStrat, fieldKey)
-    elif forceOv is False and os.path.isfile(dataBase):
-        addToDB(gffFile,dataBase,idSpec,backup,mergeStrat, fieldKey)
-
-
-def createDB(gffFile, dataBase, forceOv, idSpec, mergeStrat, fieldKey):
-    # creating id specification list
-    if fieldKey != 'none':
-        ids = ":" + fieldKey + ":"
+    if inputDB:
+        addToDB(gffFile,inputDB)
     else:
-        ids = re.split(r",*",idSpec)
-    # creating a new database
-    gffutils.create_db(gffFile, dbfn=dataBase, id_spec=ids, force=forceOv, verbose=False, merge_strategy=mergeStrat, force_gff=True, keep_order=True)
+        createDB(gffFile,newDB)
 
 
-def addToDB(gffFile, dataBase, idSpec, backup, mergeStrat, fieldKey):
-    # creating id specification list
-    if fieldKey != 'none':
-        ids = ":" + fieldKey + ":"
-    else:
-        ids = re.split(r",*",idSpec)
+def addToDB(gffFile, inputDB):
     # opening the database
-    db = gffutils.FeatureDB(dbfn=dataBase)
-    db.update(gffFile, make_backup=backup, id_spec=ids, verbose=False, merge_strategy=mergeStrat)
+    try:
+        db = gffutils.FeatureDB(dbfn=inputDB)
+    except:
+        print("can't open input database")
+    try:
+        db.update(gffFile, id_spec=':source:', merge_strategy='create_unique')
+    except:
+        print("can't add files to database; source key is already in use; please change the source description with the \'gff_source_editing\' tool")
+
+
+def createDB(gffFile, newDB):
+    # creating a new database
+    gffutils.create_db(gffFile, dbfn=newDB, id_spec=':source:', merge_strategy='create_unique')
 
 
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(prog = 'gff_to_db.py', description='Create a database from a gff file or add to an excisting database.', prefix_chars='-+', epilog="")
     parser.add_argument('--version', action='version', version='%(prog)s 0.1')
-    parser.add_argument('--gff', '-g', dest='gffFile', required=True, help='GFF file')
-    parser.add_argument('--database', '-d', dest='dataBase', required=True, help='name of the database')
-    parser.add_argument('--forceOV', '-f', dest='forceOv', action='store_true', help='forcefully overwrites an excisting database, else adds the data to it')
-    parser.add_argument('--backup', '-b', dest='backup', action='store_true', help='creates a backup before adding to the database')
-    parser.add_argument('--idSpec', '-i', dest='idSpec', default='ID,Name,transcript_id,gene_id,evidence', help='a comma separated list for the construction of the primary key, the first arguments will be prioritized')
-    parser.add_argument('--merge', '-m', dest='mergeStrat', default='error', choices=['merge', 'create_unique', 'error', 'warning'], help='merge strategy for duplicates')
-    parser.add_argument('--fieldKey', '-k', dest='fieldKey', default='none', choices=['none', 'seqid', 'source', 'featuretype', 'start', 'end', 'score', 'strand', 'frame', 'attributes'], help='use GFF field value as primary key')
+    parser.add_argument('--gff', '-g', dest='gffFile', required=True, help='input GFF file')
+    parser.add_argument('--input_database', '-i', dest='inputDB', help='write to existing database')
+    parser.add_argument('--new_database', '-n', dest='newDB', help='create new database')
     
     options = parser.parse_args()
-    main(options.gffFile, options.dataBase, options.forceOv, options.backup, options.idSpec, options.mergeStrat, options.fieldKey)
-
-
-# optional:
-# ---------
-# transform: The transform kwarg is a function that accepts single gffutils.Feature object 
-#            and that returns a (possibly modified) gffutils.Feature object. It is used to 
-#            modify, on-the-fly, items as they are being imported into the database. It is 
-#            generally used for files that don't fit the standard GFF3 or GTF specs.
+    if not (options.inputDB or options.newDB):
+        parser.error("One of --input_database or --new_database must be given")
+    
+    main(options.gffFile, options.inputDB, options.newDB)
 
